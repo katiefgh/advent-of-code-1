f6dc5abb544af8da85491c936e63e293677a53ae
katiefgh <32751159+katiefgh@users.noreply.github.com>
2018-12-07 01:55:26 -0600

    Update aoc-1-1.py

--- aoc-1-1.py
+++ aoc-1-1.py
@@ -8,27 +8,21 @@ count = {0:1}
 
 while count[frequency]==1:
     for i in input_d:
-        for o in i:
-            if o == i[0]:
-                if o == '+':
-                    frequency += int(i[1:])
-                    if frequency in count:
-                        count[frequency] += 1
-                        if count[frequency] == 2:
-                            print(frequency)
-                            break
-                    else:
-                        count[frequency] = 1
-                else:
-                    frequency -= int(i[1:])
-                    if frequency in count:
-                        count[frequency] += 1
-                        if count[frequency] == 2:
-                            print(frequency)
-                            break
-                    else:
-                        count[frequency] = 1
+        if i[0] == '+':
+            frequency += int(i[1:])
+            if frequency in count:
+                count[frequency] += 1
+                if count[frequency] == 2:
+                    print(frequency)
+                    break
             else:
-                continue
-
-
+                count[frequency] = 1
+        else:
+            frequency -= int(i[1:])
+            if frequency in count:
+                count[frequency] += 1
+                if count[frequency] == 2:
+                    print(frequency)
+                    break
+            else:
+                count[frequency] = 1
